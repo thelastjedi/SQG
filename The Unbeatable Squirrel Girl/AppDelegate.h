7e068f4f4d71b34795ed881637368762d3e0c2b7
Harshita <thelastjedi@users.noreply.github.com>
2015-09-16 21:42:51 +0530

    initial commit

---
+++ The Unbeatable Squirrel Girl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Unbeatable Squirrel Girl
+//
+//  Created by Harshita on 16/09/15.
+//  Copyright © 2015 HC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Unbeatable Squirrel Girl/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  The Unbeatable Squirrel Girl
+//
+//  Created by Harshita on 16/09/15.
+//  Copyright © 2015 HC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
